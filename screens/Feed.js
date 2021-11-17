74455bb8d6908208a7ec838ee0acc6ba5b704e84
saumyamishra01 <75878141+saumyamishra01@users.noreply.github.com>
2021-11-17 20:51:25 +0530

    Update Feed.js

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,17 +1,1 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
 
-export default class Feed extends Component {
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    justifyContent: "center",
-                    alignItems: "center"
-                }}>
-                <Text>Feeds</Text>
-            </View>
-        )
-    }
-}
